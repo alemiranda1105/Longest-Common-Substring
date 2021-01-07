945622ba2b4689a2107fa3da539329ef465a1f4d
alemiranda1105 <amiranda110500@gmail.com>
2021-01-07 12:10:01 +0000

    Memoization corregido

--- Memoization.py
+++ Memoization.py
@@ -3,19 +3,21 @@
 def memoization(s1, s2):
     mem = {}
 
-    def getKey(l1, l2):
-        key = str(l1) + "|" + str(l2)
+    def getKey(l1, l2, count):
+        key = str(l1) + "|" + str(l2) + "|" + str(count)
         return key
 
-    def findLengthLCS(mem, s1, s2, l1, l2):
+    def findLengthLCS(mem, s1, s2, l1, l2, count):
+        key = getKey(l1, l2, count)
         if l1 == len(s1) or l2 == len(s2):
-            return 0
-        key = getKey(l1, l2)
+            return count
         if key not in mem:
+            c1 = count
             if s1[l1] == s2[l2]:
-                mem[key] = 1 + findLengthLCS(mem, s1, s2, l1+1, l2+1)
-            else:
-                mem[key] = max(findLengthLCS(mem, s1, s2, l1+1, l2), findLengthLCS(mem, s1, s2, l1, l2+1))
+                c1 = findLengthLCS(mem, s1, s2, l1+1, l2+1, count+1)
+            c2 = findLengthLCS(mem, s1, s2, l1, l2+1, 0)
+            c3 = findLengthLCS(mem, s1, s2, l1+1, l2, 0)
+            mem[key] = max(c1, max(c2, c3))
         return mem[key]
 
-    return findLengthLCS(mem, s1, s2, 0, 0)
+    return findLengthLCS(mem, s1, s2, 0, 0, 0 )
