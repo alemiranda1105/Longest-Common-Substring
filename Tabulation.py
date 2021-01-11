194e2fabff7bf06afe7eb97720729aba181e4456
alemiranda1105 <amiranda110500@gmail.com>
2021-01-11 11:42:29 +0000

    Tabulation corregido

--- Tabulation.py
+++ Tabulation.py
@@ -4,14 +4,14 @@ import numpy as np
 def tabulation(s1, s2):
     table = np.zeros((len(s1)+1, len(s2)+1))
     result = 0
-    for i in range(0, len(s1)):
-        for j in range(0, len(s2)):
+    for i in range(0, len(s1)+1):
+        for j in range(0, len(s2)+1):
             if i == 0 or j == 0:
-                table[i+1][j+1] = 0
-            elif s1[i] == s2[j]:
-                table[i+1][j+1] = table[i][j] + 1
-                result = max(result, table[i+1][j+1])
+                table[i][j] = 0
+            elif s1[i-1] == s2[j-1]:
+                table[i][j] = table[i-1][j-1] + 1
+                result = max(result, table[i][j])
             else:
-                table[i+1][j+1] = 0
+                table[i][j] = 0
     # Coversión a Int para evitar que devuelva un resultado.0
     return int(result)
